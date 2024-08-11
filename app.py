992054abae5556753074dc62ae5ba677668a3e11
cagriefe <cagriefe34@gmail.com>
2024-08-11 15:43:15 +0300

    Read operation

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ from flask import Flask, redirect, request, render_template, url_for
 app = Flask(__name__)
 
 # Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
 # Read operation
+
+@app.route("/")
+def get_transactions():
+    return render_template("transactions.html", transactions=transactions)
 
 # Create operation
 
